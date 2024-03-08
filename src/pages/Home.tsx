0ae7b531a7205328e0757fc623e1e12a431dd079
Naman Rastogi <54805601+namanrastogi951@users.noreply.github.com>
2024-03-08 12:42:14 +0530

    fixed title

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -89,7 +89,7 @@ const Home = () => {
               color: "primary.main",
             }}
           >
-            {title}
+            {prefix}
           </Typography>
           <Typography
             sx={{
